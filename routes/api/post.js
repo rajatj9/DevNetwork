0e523a0611034f0fa22df96400a03c2ac72a4d6f
Rajat Jain <jain.rajat50@gmail.com>
2020-05-23 02:40:04 +0530

    like and comment feature

--- routes/api/post.js
+++ routes/api/post.js
@@ -38,7 +38,7 @@ router.post(
 			await post.save();
 			res.json(post);
 		} catch (err) {
-			console.log(err);
+			console.log(err.message);
 			res.status(500).send('Server Error!');
 		}
 	},
@@ -52,7 +52,7 @@ router.get('/', auth, async (req, res) => {
 		const posts = await Post.find().sort({ date: -1 });
 		res.json(posts);
 	} catch (err) {
-		console.log(err);
+		console.log(err.message);
 		res.status(500).send('Server Error!');
 	}
 });
@@ -68,7 +68,7 @@ router.get('/:id', auth, async (req, res) => {
 		}
 		res.json(post);
 	} catch (err) {
-		console.log(err);
+		console.log(err.message);
 
 		if (err.kind == 'ObjectId') {
 			return res.status(404).json({ msg: 'Post not found.' });
@@ -94,6 +94,134 @@ router.delete('/:id', auth, async (req, res) => {
 		}
 		await post.remove();
 		res.json({ msg: 'Post removed!' });
+	} catch (error) {
+		console.log(error);
+
+		if (error.kind == 'ObjectId') {
+			return res.status(404).json({ msg: 'Post not found.' });
+		}
+
+		res.status(500).send('Server Error!');
+	}
+});
+
+// @route PUT /api/post/like/:id
+// @desc  like a post
+// @access Private
+router.put('/like/:id', auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		// Check if the post has already been liked
+		if (post.likes.some(like => like.user.toString() === req.user.id)) {
+			return res.status(400).json({ msg: 'Post already liked' });
+		}
+
+		post.likes.unshift({ user: req.user.id });
+
+		await post.save();
+
+		return res.json(post.likes);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
+
+// @route PUT /api/post/unlike/:id
+// @desc  unlike a post
+// @access Private
+router.put('/unlike/:id', auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		// Check if the post has already been liked
+		if (!post.likes.some(like => like.user.toString() === req.user.id)) {
+			return res.status(400).json({ msg: 'Post not liked yet' });
+		}
+
+		// Get remove index
+		const removeIndex = post.likes
+			.map(like => like.user.toString())
+			.indexOf(req.user.id);
+		post.likes.splice(removeIndex, 1);
+		await post.save();
+
+		return res.json(post.likes);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
+
+// @route  POST api/post/comment/:id
+// @desc   Add new comment on post
+// @access Private
+router.post(
+	'/comment/:id',
+	[
+		auth,
+		[
+			check('text', 'Post cannot be empty')
+				.not()
+				.isEmpty(),
+		],
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			res.status(400).json({ errors: errors.array() });
+		}
+
+		try {
+			const post = await Post.findById(req.params.id);
+			const user = await User.findById(req.user.id).select('-password');
+			const newComment = {
+				text: req.body.text,
+				name: user.name,
+				avatar: user.avatar,
+				user: req.user.id,
+			};
+			post.comments.unshift(newComment);
+			await post.save();
+			res.json(post.comments);
+		} catch (err) {
+			console.log(err.message);
+			res.status(500).send('Server Error!');
+		}
+	},
+);
+
+// @route DELETE /api/post/comment/:id/:comment_id
+// @desc  Delete comment
+// @access Private
+
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		// Retrieve comment
+
+		const comment = post.comments.find(
+			comment => comment.id === req.params.comment_id,
+		);
+
+		if (!comment) {
+			return res.status(404).json({ msg: 'Comment does not exist' });
+		}
+
+		if (comment.user.id.toString() == req.user.id) {
+			return res.status(401).json({ msg: 'User not authorized' });
+		}
+
+		const removeIndex = post.comments
+			.map(comment => comment.user.toString())
+			.indexOf(req.user.id);
+
+		post.comments.splice(removeIndex, 1);
+
+		await post.save();
+		res.json(post.comments);
 	} catch (error) {
 		console.log(error);
 

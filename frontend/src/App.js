cd9b35466edeee74204e67ba0612fbd663457059
Rajat Jain <jain.rajat50@gmail.com>
2020-05-23 15:57:50 +0530

    frontend init

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import './App.css';
+import NavBar from './components/layout/NavBar';
+import Landing from './components/layout/Landing';
+
+const App = () => (
+	<Fragment>
+		<NavBar />
+		<Landing />
+	</Fragment>
+);
+export default App;

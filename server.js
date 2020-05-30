ce9c316af9b64a446403e5b4b5243140a0e1b928
Rajat Jain <jain.rajat50@gmail.com>
2020-05-31 00:59:56 +0530

    prepare heroku deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -10,13 +11,21 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
-
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/post', require('./routes/api/post'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/auth', require('./routes/api/auth'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV == 'production') {
+	// Set Static Folder
+	app.use(express.static('frontend/build'));
+
+	app.use('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 
